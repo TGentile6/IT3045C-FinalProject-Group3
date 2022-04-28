e197f17fc4f318e05e7162b6e0c10ce89c88fd4b
Emma <brantled@mail.uc.edu>
2022-04-28 11:38:20 -0400

    tables

---
+++ Tables/FavTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NULL PRIMARY KEY, 
+    [Hometown] NVARCHAR(50) NOT NULL, 
+    [State] NVARCHAR(2) NOT NULL, 
+    [HS] NVARCHAR(50) NOT NULL, 
+    [HSGradYear] INT NOT NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([Id])
+)
