054b774e66514b7b6fcc9ffaf0e473295aebe00f
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-04-28 17:53:35 -0400

    Removed Tables directory

--- Tables/FavoritesTable.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NULL PRIMARY KEY, 
-    [FavFood] NVARCHAR(50) NOT NULL, 
-    [FavFruit] NVARCHAR(50) NOT NULL, 
-    [FavColor] NVARCHAR(50) NOT NULL, 
-    [FavIceCream] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_Table] PRIMARY KEY ([Id])
-)
--- Tables/HistoryTable.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NULL PRIMARY KEY, 
-    [Hometown] NVARCHAR(50) NOT NULL, 
-    [State] NVARCHAR(2) NOT NULL, 
-    [HS] NVARCHAR(50) NOT NULL, 
-    [HSGradYear] INT NOT NULL, 
-    CONSTRAINT [PK_Table] PRIMARY KEY ([Id])
-)
